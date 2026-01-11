dc48d716a07360da1837a4c8309dc8157ea0f188
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-01-11 03:36:27 -0500

    update

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-the-resurrection-of-jesus",
+      "name": "1. The Resurrection of Jesus",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2063842656550996,
+          "pitch": -0.4212175236214257,
+          "rotation": 0,
+          "target": "1-2-the-ascension-of-jesus"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.16963560604543382,
+          "pitch": -0.7161047132627623,
+          "title": "Jesus Has Risen",
+          "text": "After the Sabbath, at dawn on the first day of the week, Mary Magdalene and the other Mary went to look at the tomb.<div><br></div><div>There was a violent earthquake, for an angel of the Lord came down from heaven and, going to the tomb, rolled back the stone and sat on it. His appearance was like lightning, and his clothes were white as snow. The guards were so afraid of him that they shook and became like dead men.</div><div><br></div><div>The angel said to the women, “Do not be afraid, for I know that you are looking for Jesus, who was crucified. He is not here; he has risen, just as he said. Come and see the place where he lay. Then go quickly and tell his disciples: ‘He has risen from the dead and is going ahead of you into Galilee. There you will see him.’ Now I have told you.”</div><div><br></div><div>So the women hurried away from the tomb, afraid yet filled with joy, and ran to tell his disciples. Suddenly Jesus met them. “Greetings,” he said. They came to him, clasped his feet and worshiped him. Then Jesus said to them, “Do not be afraid. Go and tell my brothers to go to Galilee; there they will see me.”</div><div>(Matthew 28: 1–10)</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-the-ascension-of-jesus",
+      "name": "2. The Ascension of Jesus",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8096119890518985,
+          "pitch": -0.19106218546817644,
+          "rotation": 0,
+          "target": "2-3-the-descending-of-the-holy-spirit"
+        },
+        {
+          "yaw": 2.951832376943085,
+          "pitch": 0.16029364471514995,
+          "rotation": 0,
+          "target": "0-1-the-resurrection-of-jesus"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8073577338111999,
+          "pitch": -0.5330798940881074,
+          "title": "Jesus Taken Up Into Heaven",
+          "text": "In my former book, Theophilus, I wrote about all that Jesus began to do and to teach until the day he was taken up to heaven, after giving instructions through the Holy Spirit to the apostles he had chosen. After his suffering, he presented himself to them and gave many convincing proofs that he was alive. He appeared to them over a period of forty days and spoke about the kingdom of God. On one occasion, while he was eating with them, he gave them this command: “Do not leave Jerusalem, but wait for the gift my Father promised, which you have heard me speak about. For John baptized with water, but in a few days you will be baptized with the Holy Spirit.”<div><br></div><div>Then they gathered around him and asked him, “Lord, are you at this time going to restore the kingdom to Israel?”</div><div><br></div><div>He said to them: “It is not for you to know the times or dates the Father has set by his own authority. But you will receive power when the Holy Spirit comes on you; and you will be my witnesses in Jerusalem, and in all Judea and Samaria, and to the ends of the earth.”</div><div><br></div><div>After he said this, he was taken up before their very eyes, and a cloud hid him from their sight.</div><div><br></div><div>They were looking intently up into the sky as he was going, when suddenly two men dressed in white stood beside them. “Men of Galilee,” they said, “why do you stand here looking into the sky? This same Jesus, who has been taken from you into heaven, will come back in the same way you have seen him go into heaven.” (Acts 1:10-11)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-the-descending-of-the-holy-spirit",
+      "name": "3. The Descending of the Holy Spirit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07797276115379503,
+          "pitch": -0.07289250627504629,
+          "rotation": 0,
+          "target": "3-4-the-assumption-of-mary"
+        },
+        {
+          "yaw": 1.8883506259534517,
+          "pitch": -0.0403049612876476,
+          "rotation": 0,
+          "target": "1-2-the-ascension-of-jesus"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1220619620276775,
+          "pitch": -0.3879939574094422,
+          "title": "The Holy Spirit Comes at Pentecost",
+          "text": "When the day of Pentecost came, they were all together in one place. Suddenly a sound like the blowing of a violent wind came from heaven and filled the whole house where they were sitting. They saw what seemed to be tongues of fire that separated and came to rest on each of them. All of them were filled with the Holy Spirit and began to speak in other tongues as the Spirit enabled them.&nbsp;<div>(Acts 2:1-4)</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-the-assumption-of-mary",
+      "name": "4. The Assumption of Mary",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2935011818993001,
+          "pitch": -0.37154560321026864,
+          "rotation": 0,
+          "target": "4-5-the-coronation-of-the-blessed-virgin-mary"
+        },
+        {
+          "yaw": 2.3811229786583112,
+          "pitch": 0.15891166335460127,
+          "rotation": 0,
+          "target": "2-3-the-descending-of-the-holy-spirit"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2749162830822307,
+          "pitch": -0.539810738501,
+          "title": "Mary’s Song",
+          "text": "And Mary said:<div><br></div><div>&nbsp;“My soul glorifies the Lord</div><div>and my spirit rejoices in God my Savior, for he has been mindful of the humble state of his servant.</div><div><br></div><div>From now on all generations will call me blessed, for the Mighty One has done great things for me—holy is his name.&nbsp;</div><div><br></div><div>His mercy extends to those who fear him, from generation to generation.&nbsp;</div><div><br></div><div>He has performed mighty deeds with his arm; he has scattered those who are proud in their inmost thoughts.</div><div><br></div><div>He has brought down rulers from their thrones but has lifted up the humble.&nbsp;</div><div><br></div><div>He has filled the hungry with good things but has sent the rich away empty.&nbsp;</div><div><br></div><div>He has helped his servant Israel, remembering to be merciful to Abraham and his descendants forever, just as he promised our ancestors.”&nbsp;</div><div>(Luke 1:46-55)</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-the-coronation-of-the-blessed-virgin-mary",
+      "name": "5. The Coronation of the Blessed Virgin Mary",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7484817513805355,
+          "pitch": 0.059047776227103554,
+          "rotation": 0,
+          "target": "3-4-the-assumption-of-mary"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6276083865122981,
+          "pitch": -0.20057270746461953,
+          "title": "The Woman and the Dragon",
+          "text": "A great sign appeared in heaven: a woman clothed with the sun, with the moon under her feet and a crown of twelve stars on her head. She was pregnant and cried out in pain as she was about to give birth. Then another sign appeared in heaven: an enormous red dragon with seven heads and ten horns and seven crowns on its heads. Its tail swept a third of the stars out of the sky and flung them to the earth. The dragon stood in front of the woman who was about to give birth, so that it might devour her child the moment he was born. She gave birth to a son, a male child, who “will rule all the nations with an iron scepter.” And her child was snatched up to God and to his throne. The woman fled into the wilderness to a place prepared for her by God, where she might be taken care of for 1,260 days.<div><br></div><div>Then war broke out in heaven. Michael and his angels fought against the dragon, and the dragon and his angels fought back. But he was not strong enough, and they lost their place in heaven. The great dragon was hurled down—that ancient serpent called the devil, or Satan, who leads the whole world astray. He was hurled to the earth, and his angels with him.</div><div><br></div><div>Then I heard a loud voice in heaven say:</div><div><br></div><div>“Now have come the salvation and the power and the kingdom of our God, and the authority of his Messiah. For the accuser of our brothers and sisters, who accuses them before our God day and night, has been hurled down.</div><div><br></div><div>They triumphed over him by the blood of the Lamb and by the word of their testimony; they did not love their lives so much as to shrink from death.</div><div><br></div><div>Therefore rejoice, you heavens and you who dwell in them! But woe to the earth and the sea, because the devil has gone down to you! He is filled with fury, because he knows that his time is short.”</div><div><br></div><div>When the dragon saw that he had been hurled to the earth, he pursued the woman who had given birth to the male child. The woman was given the two wings of a great eagle, so that she might fly to the place prepared for her in the wilderness, where she would be taken care of for a time, times and half a time, out of the serpent’s reach. Then from his mouth the serpent spewed water like a river, to overtake the woman and sweep her away with the torrent. But the earth helped the woman by opening its mouth and swallowing the river that the dragon had spewed out of his mouth. Then the dragon was enraged at the woman and went off to wage war against the rest of her offspring—those who keep God’s commands and hold fast their testimony about Jesus.</div><div>(Revelation 12:1-17)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
